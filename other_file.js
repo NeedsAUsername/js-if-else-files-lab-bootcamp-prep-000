b6fbca019faf567999ab470bbeab9c49d66b0aba
quip550@gmail.com Chi <quip550@gmail.com>
2018-05-20 17:49:28 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -1,3 +1,4 @@
 var index = require("./index.js") //transfers data from index.js so we can use those variables in this file too
+index.height = index.height + 10
 console.log(index.name);
 console.log(index.height);
